53593869d98a42aacbc185086c6dcb2e2119ae5e
temirovuz <temirov-muhammad@amil.ru>
2025-08-29 16:41:47 +0500

    udate urls code

--- exchange/api/v1/urls.py
+++ exchange/api/v1/urls.py
@@ -5,10 +5,10 @@ from exchange.api.v1.views import PartnerListCreateView, PartnerUpdateView, Tran
 
 urlpatterns = [
     path('partners/', PartnerListCreateView.as_view(), name='partner-create-list'),
-    path('partber/<int:pk>', PartnerUpdateView.as_view, name='partner-edit'),
+    path('partner/<int:pk>/', PartnerUpdateView.as_view(), name='partner-edit'),
     path('transaction/', TransactionListCreateView.as_view(), name='transaction-create-list'),
     path('transaction/<int:pk>', TransactionDetailView.as_view(), name='transaction-create-list'),
 
     path('product_order/', ProductOrderListCreateView.as_view(), name='transaction-create-list'),
-    path('product_order/<int:pk>', ProductOrderDetailView.as_view(), name='transaction-create-list'),
+    path('product_order/<int:pk>/', ProductOrderDetailView.as_view(), name='transaction-create-list'),
 ]
