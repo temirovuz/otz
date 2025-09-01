6a80a23d2f0e14a784a56a2dce8038934543f9be
temirovuz <temirov-muhammad@amil.ru>
2025-09-01 08:49:24 +0500

    update code

--- core/settings.py
+++ core/settings.py
@@ -164,7 +164,7 @@ LOGGING = {
 
 # JWT Settings
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
